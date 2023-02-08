35bc0ecc4d55bf4c04e73bdfe775aa4f69413125
Jb <jb@levillageducode.fr>
2023-02-08 11:17:41 +0100

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
+import {Routes,Route} from 'react-router-dom';
+
 import './App.css';
 import Users from "./exo1/Users";
 import Tasks from "./exo1/Tasks";
 
 function App() {
-
-
   return (
-    <div className="App">
-      <Tasks />
-      {/* <Users /> */}
-    </div>
+    <Routes>
+      <Route path="/" element={<Tasks/>} />
+      <Route path="users" element={<Users />} />
+    </Routes>
   );
 }
 
